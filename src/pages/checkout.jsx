5292de523ea5c30a7cd05e954e9c6648ec252423
Mohaamad Kasim <kasimwasi111@gmail.com>
2023-09-21 01:04:21 +0530

    done

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -4,145 +4,38 @@ import { Link } from "react-router-dom";
 // import { ShopContext } from "../components/shopcontext";
 
 const checkout = () => {
-  // const { getTotalCartProducts, getTotalCartAmount, resetCart } =
-  //   useContext(ShopContext);
-  // const totalAmount = getTotalCartAmount();
-  // const totalProducts = getTotalCartProducts();
-
-  // const handlePay = () => {
-  //   window.alert(
-  //     `Thank you for your purchase of ${totalProducts} products for a total of $${totalAmount}. Your request has been received and is being processed.`
-  //   );
-  //   resetCart();
-  // };
-
   return (
     <>
       <section className="checkout p-5">
         <div className="container-sm ">
           <div className="row d-flex justify-content-center">
-            {/* <div className="col-md-6">
-              <h1 className="mb-4 fs-3">Payment Method</h1>
-              <div className="accordion" id="accordionExample">
-                <div className="card">
-                  <div className="card-header p-0" id="headingTwo">
-                    <button
-                      className="btn col-12 btn-light btn-block text-start collapsed p-3 rounded-0 border-bottom-custom"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseTwo"
-                      aria-expanded="false"
-                      aria-controls="collapseTwo"
-                    >
-                      <div className="d-flex align-items-center justify-content-between">
-                        <div className="col-6">
-                          <span>Paypal</span>
-                        </div>
-                        <div className="col-6">
-                          <img src={pay} alt="" className="img-fluid" />
-                        </div>
-                      </div>
-                    </button>
-                  </div>
-                  <div
-                    id="collapseTwo"
-                    className="collapse"
-                    aria-labelledby="headingTwo"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div className="card-body">
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Paypal email"
-                        required
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="card m-auto">
-                  <div className="card-header p-0">
-                    <button
-                      className="btn col-12 btn-light btn-block text-start p-3 rounded-0"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseOne"
-                      aria-expanded="true"
-                      aria-controls="collapseOne"
-                    >
-                      <div className="d-flex align-items-center justify-content-between">
-                        <div className="col-6">
-                          <span>Credit card</span>
-                        </div>
-                        <div className="icons col-6">
-                          <img src={pay} alt="" className="img-fluid" />
-                        </div>
-                      </div>
-                    </button>
-                  </div>
-                  <div
-                    id="collapseOne"
-                    className="collapse show"
-                    aria-labelledby="headingOne"
-                    data-bs-parent="#accordionExample"
-                  >
-                    <div className="card-body payment-card-body">
-                      <span className="font-weight-normal card-text">
-                        Card Number
-                      </span>
-                      <div className="input mb-4">
-                        <i className="fa fa-credit-card"></i>
-                        <input
-                          type="text"
-                          className="form-control"
-                          placeholder="0000 0000 0000 0000"
-                          required
-                        />
-                      </div>
-                      <div className="row mt-3 mb-3">
-                        <div className="col-md-6">
-                          <span className="font-weight-normal card-text">
-                            Expiry Date
-                          </span>
-                          <div className="input">
-                            <i className="fa fa-calendar"></i>
-                            <input
-                              type="text"
-                              className="form-control"
-                              placeholder="MM/YY"
-                              required
-                            />
-                          </div>
-                        </div>
-                        <div className="col-md-6">
-                          <span className="font-weight-normal card-text">
-                            CVC/CVV
-                          </span>
-                          <div className="input mb-4">
-                            <i className="fa fa-lock"></i>
-                            <input
-                              type="text"
-                              className="form-control"
-                              placeholder="000"
-                              required
-                            />
-                          </div>
-                        </div>
-                      </div>
-                      <span className="text-muted certificate-text">
-                        <i className="fa fa-lock"></i> Your transaction is
-                        secured with ssl certificate
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div> */}
-
             <div className="col-md-6 p-2">
               <h1 className=" mt-3 mb-3 fs-3">
                 Fill the following details for shipping.
               </h1>
               <form className="row g-3 mb-3">
+                <div className="col-md-6">
+                  <label htmlFor="fName" className="form-label">
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="fName"
+                    required
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label htmlFor="lName" className="form-label">
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="lName"
+                    required
+                  />
+                </div>
                 <div className="col-md-6">
                   <label htmlFor="inputEmail4" className="form-label">
                     Email
@@ -155,13 +48,13 @@ const checkout = () => {
                   />
                 </div>
                 <div className="col-md-6">
-                  <label htmlFor="inputPassword4" className="form-label">
-                    Password
+                  <label htmlFor="phone" className="form-label">
+                    Contact No.
                   </label>
                   <input
-                    type="password"
+                    type="phone"
                     className="form-control"
-                    id="inputPassword4"
+                    id="phone"
                     required
                   />
                 </div>
@@ -188,7 +81,7 @@ const checkout = () => {
                     placeholder="Apartment, studio, or floor"
                   />
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-5">
                   <label htmlFor="inputCity" className="form-label">
                     City
                   </label>
@@ -199,7 +92,18 @@ const checkout = () => {
                     required
                   />
                 </div>
-                <div className="col-md-2">
+                <div className="col-md-4">
+                  <label htmlFor="inputState" className="form-label">
+                    State
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="inputState"
+                    required
+                  />
+                </div>
+                <div className="col-md-3">
                   <label htmlFor="inputZip" className="form-label">
                     Zip
                   </label>
